9466fa9d185761cbca636d7ca842d7d920a0ad97
Harry Johnson <hcjiv1@renci.org>
2014-04-03 18:51:53 +0000

    [#3] Try a different invocation of grid-proxy-init

--- packaging/test_irods_auth_plugin_gsi.py
+++ packaging/test_irods_auth_plugin_gsi.py
@@ -79,7 +79,7 @@ class Test_Gsi_Suite(unittest.TestCase, ResourceBase):
 
         # Make sure we have a valid proxy cert
         os.system("echo %s > password_file" % gsiPassword)
-        os.system("grid-proxy-init -pwstdin < password_file")
+        os.system("grid-proxy-init -pwstdin")
 
         # Try an ils
         assertiCmd(s.adminsession, "ils", "LIST", "home")
