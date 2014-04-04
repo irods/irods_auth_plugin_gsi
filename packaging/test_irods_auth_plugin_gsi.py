28f94a9c6714c96b681c44d32afeb19975d2692c
Harry Johnson <hcjiv1@renci.org>
2014-04-04 16:36:52 +0000

    [#3] Test code

--- packaging/test_irods_auth_plugin_gsi.py
+++ packaging/test_irods_auth_plugin_gsi.py
@@ -80,7 +80,7 @@ class Test_Gsi_Suite(unittest.TestCase, ResourceBase):
         # Make sure we have a valid proxy cert
         pipe = os.popen("grid-proxy-init -pwstdin > /dev/null", 'w')
         print("Writing password: %s to the grid-proxy-init" % gsiPassword)
-        pipe.write(gsiPassword)
+        pipe.write("irods")
         pipe.close()
 
         # Try an ils
